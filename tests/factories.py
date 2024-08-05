5713148766a9ab9e7c40a1e37643b085deda718a
Syed Mohsin Ali Naqvi <syedmohsin1@live.com>
2024-08-05 19:46:36 +0200

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -1,3 +1,4 @@
+
 # Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,3 +33,40 @@ class ProductFactory(factory.Factory):
 
     id = factory.Sequence(lambda n: n)
    ## Add code to create Fake Products 
+class ProductFactory(factory.Factory):
+    """Creates fake products for testing"""
+
+    class Meta:
+        """Maps factory to data model"""
+
+        model = Product
+
+    id = factory.Sequence(lambda n: n)
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
